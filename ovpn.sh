60d6d3ec55d9f59d33e785021f9341acc22a9411
polihoster <polihoster@gmail.com>
2020-01-04 09:43:35 +0200

    Update ovpn.sh

--- ovpn.sh
+++ ovpn.sh
@@ -3,9 +3,10 @@
 TMDIR=/tmp/ovpn
 mkdir $TMDIR
 cd $TMDIR
- 
+
+service openvpn stop
 apt-get install -y wget
 wget --no-check-certificate $1 -O - | tar -x
 mv  $TMDIR/*.ovpn /etc/openvpn/client.conf
-service openvpn restart
+service openvpn start
 echo OK
